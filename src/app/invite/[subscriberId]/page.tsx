912ea4ff78b769d2b5eddfb4be0cf52d1c3797f7
mariliafranco <marilia.andrad@gmail.com>
2025-03-11 17:37:06 -0300

    Add orval and handle api integration

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -38,7 +38,7 @@ export default async function InvitePage(props: InvitePageProps) {
                     </div>
 
                     <InviteLinkInput inviteLink={inviteLink} />
-                    <Stats />
+                    <Stats subscriberId={subscriberId} />
 
                 </div>
             </div>
--- src/app/invite/[subscriberId]/stats.tsx
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -1,10 +1,19 @@
-import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react';
+import { getGetSubscriberInviteClicksUrl, getSubscriberInviteClicks, getSubscriberInviteCount, getSubscriberRankingPosition } from '@/http/api';
+import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react';
 
-export function Stats() {
+interface StatsProps {
+    subscriberId: string;
+}
+
+export async function Stats({ subscriberId }: StatsProps) {
+    const { count: accessCount } = await getSubscriberInviteClicks(subscriberId);
+    const { count: inviteCount } = await getSubscriberInviteCount(subscriberId);
+    const { position: rankingPosition } = await getSubscriberRankingPosition(subscriberId);
+
     return (
         <div className='grid gap-3 md:grid-cols-3'>
             <div className='relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl'>
-                <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>1042</span>
+                <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>{accessCount}</span>
                 <span className='text-sm text-gray-300 leading-none text-center'> Acessos ao link</span>
 
                 <MousePointerClick className='size-5 text-purple absolute top-3 left-3' />
@@ -12,7 +21,7 @@ export function Stats() {
 
 
             <div className='relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl'>
-                <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>1042</span>
+                <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>{inviteCount}</span>
                 <span className='text-sm text-gray-300 leading-none text-center'> Inscrições Feitas</span>
 
                 <BadgeCheck className='size-5 text-purple absolute top-3 left-3' />
@@ -20,8 +29,8 @@ export function Stats() {
             </div>
 
             <div className='relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl'>
-                <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>1042</span>
-                <span className='text-sm text-gray-300 leading-none text-center'> Acessos ao link</span>
+                <span className='font-heading text-2xl font-semibold text-gray-200 leading-none'>{rankingPosition ? `${rankingPosition}º` : '-'}</span>
+                <span className='text-sm text-gray-300 leading-none text-center'>Posição no Ranking</span>
 
                 <Medal className='size-5 text-purple absolute top-3 left-3' />
 
